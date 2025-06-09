ce013023e2028a9e2949a0b1b1e799871df0376f
Valentinomarcos <valentinocecco999@gmail.com>
2025-06-09 20:32:59 -0300

    Add files via upload

---
+++ notas.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+ int main(void){
+	 
+	 int nota;
+	 
+	printf("ingrese una nota  ");
+	scanf("%d", &nota);
+	
+	if ( nota >= 90 )
+	{
+		printf("calificacion A");
+	}
+	else if ( nota >= 80 )
+	{
+		printf (" calificacion B ");
+	}
+	else if ( nota >= 70 )
+	{
+		printf(" calificacion C ");
+	}
+	else if ( nota >= 60 )
+	 {
+		 printf(" calificacion D ");
+	 }
+	else 
+		{
+		 printf(" Calificacion F ");
+	 }
+	
+	return 0;
+}
